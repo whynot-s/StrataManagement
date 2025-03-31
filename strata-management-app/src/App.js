da485b9ace2f337dcc2d137c83aba94887857ffb
Jack Wang <whynot_s@outlook.com>
2025-03-31 09:59:06 +0800

    add strata functions

--- strata-management-app/src/App.js
+++ strata-management-app/src/App.js
@@ -1,14 +1,33 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const apartments = [
+    {id: 1, name: 'Apartment A', location: 'City Center', reportURL: '/ApaA_Report.pdf'},
+    {id: 2, name: 'Apartment B', location: 'Down Town', reportURL: '/ApaB_Report.pdf'},
+    {id: 3, name: 'Apartment C', location: 'Countryside', reportURL: '/ApaC_Report.pdf'},
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          <h1>Strata Management</h1>
-          </div>
-      </header>
+      <div>
+        <h1>Strata Management</h1>
+        <h2>Apartment List</h2>
+          {
+            apartments.map(apartment => (
+              <div key={apartment.id}> 
+               {apartment.name} - {apartment.location} <a href={apartment.reportURL}>Download Report</a>
+               <ul>
+                <li>Electricity</li>
+                <li>Water</li>
+                <li>Heat</li>
+                <li>Gas</li>
+               </ul>
+              </div>
+            ))
+          }
+      </div>
     </div>
   );
 }
